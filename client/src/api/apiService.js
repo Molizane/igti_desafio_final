bf9c0daa57d901c7ea5b8456630b5a9bf1ce39ac
Angelo Molizane <molizane@gmail.com>
2021-02-08 14:15:59 -0300

    Heroku 3

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -8,21 +8,21 @@ export async function getPeriod(period) {
     };
   }
 
-  const res = await axios.get(`${process.env.REACT_APP_APIURL}?period=${period}`);
+  const res = await axios.get(`/api/transaction/?period=${period}`);
   return res.data;
 }
 
 export async function remove(id) {
-  const res = await axios.delete(`${process.env.REACT_APP_APIURL}?id=${id}`);
+  const res = await axios.delete(`/api/transaction/?id=${id}`);
   return res.data;
 }
 
 export async function insert(transaction) {
-  const res = await axios.post(`${process.env.REACT_APP_APIURL}`, transaction);
+  const res = await axios.post('/api/transaction/', transaction);
   return res.data;
 }
 
 export async function update(transaction, _id) {
-  const res = await axios.put(`${process.env.REACT_APP_APIURL}?id=${_id}`, transaction);
+  const res = await axios.put(`/api/transaction/?id=${_id}`, transaction);
   return res.data;
 }
